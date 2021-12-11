7ce275c9a60bc4dfcc5b5dcab0a14cbd7a920033
CarolynnFleming <cfcatpink@gmail.com>
2021-12-10 16:52:29 -0800

    yes

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,21 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderZombie } from "../render-utils.js";
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
+let zombieData = 
+  {name: 'Chad', hp: 2};
+
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div class="zombie"><p>Chad</p><p>🧟‍♂️</p><p>2</p></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderZombie(zombieData).outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual, expected, '');
 });
